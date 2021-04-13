88d7bd095c66b1b3bb115888da729509e6c68fff
Kaique Moreira <kaiquemoreiracloud@gmail.com>
2021-04-13 20:40:21 -0300

    CRUD Scaffolding
    Created CRUD Scaffolding
    Controllers > Add > New Scaffolding Item

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models.ViewModels;
+
+namespace SalesWebMvc.Data
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc.Models.ViewModels.Department> Department { get; set; }
+    }
+}
